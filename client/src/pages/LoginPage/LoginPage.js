be5d96e3d0dde7a7ac7f57859e5e01bc710f6d78
denysbialy <dnsbls11@gmail.com>
2022-05-31 20:18:29 +0300

    Fixed signup page link

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import Logo from '../../components/Logo';
-import styles from './LoginPage.module.sass';
-import { clearErrorSignUpAndLogin } from '../../actions/actionCreator';
-import CONSTANTS from '../../constants';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import Logo from "../../components/Logo";
+import styles from "./LoginPage.module.sass";
+import { clearErrorSignUpAndLogin } from "../../actions/actionCreator";
+import CONSTANTS from "../../constants";
 
 const LoginPage = (props) => (
   <div className={styles.mainContainer}>
     <div className={styles.loginContainer}>
       <div className={styles.headerSignUpPage}>
         <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo" />
-        <div className={styles.linkLoginContainer}>
-          <Link
-            to="/registration"
-            style={{ textDecoration: 'none' }}
-          >
-            <span>Signup</span>
-          </Link>
-        </div>
+        <Link to="/registration" style={{ textDecoration: "none" }}>
+          <div className={styles.linkLoginContainer}>Signup</div>
+        </Link>
       </div>
       <div className={styles.loginFormContainer}>
         <LoginForm history={props.history} />
