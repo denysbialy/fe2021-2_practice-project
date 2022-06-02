dcd4d8d153e233600311b9a260c374246bdb5e83
denysbialy <dnsbls11@gmail.com>
2022-06-03 00:13:09 +0300

    Add new page pricing list

---
+++ client/src/components/PricingList/PricingList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import priceList from './priceList.json'
+
+
+const PricingList = (props) => {
+    
+  return (<div>
+      <h2></h2>
+  </div>)
+}
+
+export default PricingList
