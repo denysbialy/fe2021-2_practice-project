f631d4acd54c7740b49829c413e94c1d69a821c9
denysbialy <dnsbls11@gmail.com>
2022-06-08 23:15:52 +0300

    Add fixed styles

--- client/src/components/PricingList/PricingList.js
+++ client/src/components/PricingList/PricingList.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import priceList from './priceList.json'
 import styles from './pricingPage.module.sass'
+import cx from 'classnames'
 
 const PricingList = props => {
+
   const styless = (index, priceList) =>
     index !== priceList.length - 1 ? {} : { borderBottom: '2px solid #fff' }
 
@@ -11,43 +13,26 @@ const PricingList = props => {
     <>
       <main className={styles.container}>
         <ul className={styles.ulPrice}>
-          {priceList.map((item, index) => {
+          {priceList.map((elem, index) => {
             return (
               <li className={styles.pricingShow}>
-                <div
-                  style={{ borderColor: item.color }}
-                  className={styles.containerPrice}
-                >
+                <div style={{ borderColor: elem.color }}className={styles.containerPrice}>
                   <div className={styles.texts}>
-                    <h2 style={{ color: item.color }}>{item.mode}</h2>
-                    <p>{item.description}</p>
+                    <h2 style={{ color: elem.color }}>{elem.mode}</h2>
+                    <p>{elem.description}</p>
                   </div>
                   <div className={styles.pricing}>
-                    <div
-                      style={{ color: item.color }}
-                      className={styles.currency}
-                    >
-                      {item.currency}
-                    </div>
-                    <div style={{ color: item.color }} className={styles.price}>
-                      {item.price}
-                    </div>
+                    <div style={{ color: elem.color }} className={styles.currency}>{elem.currency} </div>
+                    <div style={{ color: elem.color }} className={styles.price}>{elem.price}</div>
                   </div>
                 </div>
                 <ul>
-                  <li className={styles.pricingBox} style={styless(index, priceList)}>
-                    {item.prize}
-                  </li>
-                  <li className={styles.pricingBox} style={styless(index, priceList)}>{item.validation}</li>
-                  <li className={styles.pricingBox} style={styless(index, priceList)}> {item.entries}</li>
+                  <li className={styles.pricingBox}style={styless(index, priceList)}>{elem.prize}</li>
+                  <li className={styles.pricingBox}style={styless(index, priceList)}>{elem.validation}</li>
+                  <li className={cx(styles.pricingBox, styles.bordersBottom)}style={styless(index, priceList)}>{elem.entries}</li>
                 </ul>
                 <Link to='#'>
-                  <button
-                    style={{ backgroundColor: item.color }}
-                    className={styles.buttonStart}
-                  >
-                    Start
-                  </button>
+                  <button style={{ backgroundColor: elem.color }}className={styles.buttonStart}>Start</button>
                 </Link>
               </li>
             )
@@ -57,5 +42,4 @@ const PricingList = props => {
     </>
   )
 }
-
 export default PricingList
--- client/src/pages/PricingPage/PricingPage.js
+++ client/src/pages/PricingPage/PricingPage.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import PricingList from '../../components/PricingList/PricingList'
+
 const PricingPage = () => {
   return (
     <>
